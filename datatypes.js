a71875331d2cac6c0f21a859bcf907215ff2cd45
smoodhies <liyakatalishaikh946@gmail.com>
2024-05-30 17:48:38 -0400

    js 1st file commit

---
+++ datatypes.js
@@ -0,0 +1,23 @@
+"use strict"; // treat as all js code as new or latest version
+
+
+//  !! alert(3+6) !! //this is use for alert popup  message when use it it is not use in noje js but it useful in browser!!! 
+
+//ecma for read OG documentation
+
+//types of data types  premetive data types
+//number // range 2 to power of 53
+//big int // for more range in numbers
+// string // character data type  string=""
+//boolean // true or false
+//null // null means nothing empty it is a type of "object"
+//undefiend // value is not assingnedb that variable
+//symbol // use for unique identify
+
+//object
+
+console.log(typeof null);//null is a type of object
+console.log(typeof undefined);
+console.log(typeof symbol);
+console.log(typeof symbol);
+
